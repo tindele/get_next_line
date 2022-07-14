88f0c52173dce93b002050996c3413d22defcca9
Christine Penzkofer <chpenzko@1-B-15.42heilbronn.de>
2022-07-14 14:40:14 +0200

    42 14.07.

--- get_next_line.c
+++ get_next_line.c
@@ -6,41 +6,107 @@
 /*   By: chpenzko <chpenzko@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/11 10:14:32 by chpenzko          #+#    #+#             */
-/*   Updated: 2022/07/13 16:21:25 by chpenzko         ###   ########.fr       */
+/*   Updated: 2022/07/14 14:33:10 by chpenzko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
 
+char	*get_next_line(int fd)
+{
+	static char	*save;
+	char		*line;
 
-char	*read_and_save(int fd, char *save)
+	if (fd < 0  || BUFFER_SIZE <= 0)
+		return (NULL);
+	save = ft_read_and_save(fd, save);
+	if(!save)
+		return(NULL)
+	line = ft_line(save);
+	save = ft_save_rest(save);
+	return (line);
+}
+
+char	*ft_read_and_save(int fd, char	*save)
 {
 	char	*buffer;
-	int		char_count;
+	int		read_char;
 
-	buffer = (char	*) malloc((BUFFER_SIZE + 1) * sizeof(char));
-	if(!buffer)
+	buffer = (char	*) malloc ((BUFFER_SIZE + 1) * sizeof(char))
+	if (!buffer)
 		return (NULL);
-	char_count = 1;
-	while(!ft_strchr(save, '\n') && char_count != 0)
+	read_char = 1;
+	while(!ft_strchr(save, '\n') && read_char != 0)
 	{
-		char_count = read(fd, buffer, BUFFER_SIZE);
-		if (char_count == -1)
+		read_char = read(fd, buffer, BUFFER_SIZE);
+		if (read_char == -1)
 		{
 			free(buffer);
 			return (NULL);
 		}
-		buffer[char_count] = '\0';
-		save = ft_strjoin(save, buffer);
+	buffer[read_char] = '\0';
+	save = ft_strjoin(save, buffer);
 	}
 	free(buffer);
 	return (save);
 }
 
-char    *get_next_line(int fd)
+char	*ft_line(char *save)
 {
-	static char	*save;
-	char		*line;
+	int		i;
+	char	*line;
 
-	if (fd < 0 || BUFFER_SIZE <= 0)
+	if (!save)
 		return (NULL);
+	i = 0;
+	while (save[i] && save[i] != '\n')
+		i++;
+	line = (char	*) malloc((i + 2)  * sizeof(char));
+	if (!line)
+	{
+		return (NULL);
+	}
+	line = ft_strlcpy(line, save, i + 1);
+	if(save[i] == '\n')
+		line[i] == save[i];
+	line[++i] = '\0';
+	return (line);
+}
+
+char	*ft_save_rest(char	*save)
+{
+	int 	i;
+	int		j;
+	char	*rest;
+	
+	i = 0;
+	if (!save)
+		return (NULL);
+	while(save[i] && save[i] != '\n')
+		i++;
+	if(save[i] != '\n')
+	{
+		free (save);
+		return (NULL);
+	}	
+	rest = (char	*) malloc(sizeof(char) * (ft_strlen(save) - i + 1));
+	if(!rest)
+		return (NULL);
+	i++;
+	j = 0;
+	while(save[i] != '\0')
+	{
+		rest[j++] = save[i++];
+	}
+	rest[j] = '\0';
+	free (save);
+	return (rest);
+}
+
+int main()
+{
+	char	next_line[40];
+
+	next_line = get_next_line(text.txt);
+	printf("%s", next_line);
 }
